09ef321365728305b4718c8e44ce7d8e52e522c6
Jodxxx <67512684+Jodxxx@users.noreply.github.com>
2023-12-29 20:12:37 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -207,7 +207,7 @@ async def start(_, message):
     elif config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Bot Started.\n' \
                        'Now I will send all of your stuffs here.\n' \
-                       'Use me at: @Z_Mirror'
+                       'Now fu** off and use me in group'
     elif not config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Sorry, you cannot use me here!\n' \
                        'Join: @Z_Mirror to use me.\n' \
